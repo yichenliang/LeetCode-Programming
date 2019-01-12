030f1dad7579bbd6bf29b4de112f5a0a876a6201
yichenliang <yichenliang@ufl.edu>
2019-01-11 23:38:07 -0500

    added on 2019/1/11

--- 20190110-20190120/Two_Sum_1/Solution1.java
+++ 20190110-20190120/Two_Sum_1/Solution1.java
@@ -1,1 +1,20 @@
-// Solution 1
+// Approach 2: Two-pass Hash Table
+
+public int[] twoSum(int[] nums, int target) {
+	Map<Integer, Integer> map = new HashMap<>();
+	for(int i = 0; i < nums.length; i++) {
+		map.put(nums[i], i);
+	}
+	
+	for(int i = 0; i < nums.length; i++){
+		rest_value = target - nums[i];
+		if(map.containsKey(rest_value) && map.get(rest_value) != i){
+			return new int[] {i, map.get(rest_value)};
+		}
+	}
+	return new IllegalArgumentException("No two sum solution");
+	
+}
+    
+// Time complexity: O(N)
+// Space complexity: O(N)
