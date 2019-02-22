6674157df1da115e8259551ba176d0b67122e409
yichenliang <yichenliang@ufl.edu>
2019-02-22 11:26:56 -0500

    change the structure

---
+++ src/test/java/yichenliang/leetcode/PalindromePairsTest.java
@@ -0,0 +1,31 @@
+package yichenliang.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PalindromePairsTest {
+	 private  PalindromePairs solution;
+
+	    @BeforeEach
+	    void beforeEach() {
+	        solution = new PalindromePairs();
+	    }
+	    
+	    @Test
+	    void isOneBitCharacter() {
+	    	List<Integer> list1 = new ArrayList<>();
+	    	list1.add(1);
+	    	list1.add(0);
+	    	List<Integer> list2 = new ArrayList<>();
+	    	list2.add(0);
+	    	list2.add(1);
+	    	List<List<Integer>> res = new ArrayList<>();
+	    	res.add(list1);
+	    	res.add(list2);
+	        Assertions.assertEquals(res,solution.palindromePairs(new String[] {"bat","tab","cat"}));
+	    }
+}
