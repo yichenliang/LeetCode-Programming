043fbda3e48ded47f95a801e754cb5a1b882980c
yichenliang <yichenliang@ufl.edu>
2019-02-22 18:42:25 -0500

    added on 2019/2/22

--- src/test/java/yichenliang/leetcode/SlidingWindowMaximumTest.java
+++ src/test/java/yichenliang/leetcode/SlidingWindowMaximumTest.java
@@ -14,7 +14,7 @@ class SlidingWindowMaximumTest {
 
     @Test
     void test1() {
-        Assertions.assertArrayEquals(new int[] {3,3,5,5,6,8}, solution.maxSlidingWindow(new int[] { 1,3,-1,-3,5,3,6,7 }, 3));
+        Assertions.assertArrayEquals(new int[] {3,3,5,5,6,7}, solution.maxSlidingWindow(new int[] { 1,3,-1,-3,5,3,6,7 }, 3));
         
     }
 
