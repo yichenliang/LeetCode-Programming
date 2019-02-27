70bb3e0be592731be348c217dfaf0e9450a970e9
yichenliang <yichenliang@ufl.edu>
2019-02-27 17:10:48 -0500

    added on 2019/2/27

---
+++ src/main/java/yichenliang/leetcode/ReorderList.java
@@ -0,0 +1,63 @@
+package yichenliang.leetcode;
+
+/**
+ * 143. Reorder List
+ * 
+ *  Linked List
+ *  
+ *  time complexity: O(n)
+ *  space complexity: O(1)
+ */
+
+
+ 
+  class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+ 
+
+public class ReorderList {
+	public void reorderList(ListNode head) {
+        
+        if(head == null) return;
+        
+        ListNode mid = findMid(head);
+        
+        ListNode l2 = mid.next;
+        mid.next = null;
+        l2 = reverse(l2);
+        
+        ListNode l1 = head;
+        
+        while(l1 != null && l2 != null){
+            ListNode next = l1.next;
+            l1.next = l2;
+            l2 = l2.next;
+            l1.next.next =next;
+            l1 = next;   
+        }
+    }
+    
+    ListNode findMid(ListNode head){
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    
+    ListNode reverse(ListNode head){
+        ListNode newHead = null;
+        while(head != null){
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+        }
+        return newHead;
+    }
+}
---
+++ src/test/java/yichenliang/leetcode/BurstBalloonsTest.java
@@ -0,0 +1,19 @@
+package yichenliang.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BurstBalloonsTest {
+	private  BurstBalloons solution;
+
+    @BeforeEach
+    void beforeEach() {
+        solution = new BurstBalloons();
+    }
+    
+    @Test
+    void test1() {
+        Assertions.assertEquals(167,solution.maxCoins(new int[] {3,1,5,8}));
+    }
+}
