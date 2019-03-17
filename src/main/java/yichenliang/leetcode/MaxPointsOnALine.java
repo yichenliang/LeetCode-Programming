285377132aa4e8ebf9c1e3f85eb02f137e48e348
yichenliang <yichenliang@ufl.edu>
2019-03-17 17:10:38 -0400

    added on 2019/3/17

--- src/main/java/yichenliang/leetcode/MaxPointsOnALine.java
+++ src/main/java/yichenliang/leetcode/MaxPointsOnALine.java
@@ -1,9 +1,9 @@
 package yichenliang.leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
+//import java.util.HashMap;
+//import java.util.Map;
 
-import javafx.util.Pair;
+//import javafx.util.Pair;
 
 /**
  * 
@@ -23,64 +23,64 @@ import javafx.util.Pair;
 
 public class MaxPointsOnALine {
 	
-	public int maxPoints(Point[] points) {
-        if(points == null || points.length == 0) return 0;
-        
-        int n = points.length;
-        if(n == 1 || n == 2) return n;
-        
-        int ans = 0;
-        
-        for(int i = 0; i < n; i++){
-            Map<Pair<Integer, Integer>, Integer> map = new HashMap<>();
-            int samePoint = 1;
-            int max = 0; // max point in this round
-            Point p1 = points[i];
-            for(int j = i + 1; j < n; j++){
-                Point p2 = points[j];
-                
-                // same point 
-                if(p1.x == p2.x && p1.y == p2.y){
-                    samePoint++;
-                }
-                else{
-                    Pair key = getSlope(p1, p2);
-                    if(map.containsKey(key)){
-                        Integer v = map.get(key);
-                        v++;
-                        map.put(key, v);
-                    }
-                    else{
-                        map.put(key, 1);
-                    }
-                    max = Math.max(max, map.get(key));
-                } 
-                
-            }
-            ans = Math.max(ans, max + samePoint);
-        }
-        return ans;     
-    }
-    
-    Pair<Integer, Integer> getSlope(Point p1, Point p2){
-        int dx = p2.x - p1.x;
-        int dy = p2.y - p1.y;
-        
-        // horizantal line ---
-        if(dy == 0){
-            return new Pair(p1.y, 0);
-        }
-        // vertical line |
-        if(dx == 0){
-            return new Pair(0, p1.x);
-        }
-        
-        int gcd = gcd(dx, dy);
-        return new Pair(dx/gcd, dy/gcd);
-    }
-    
-    int gcd(int a, int b){
-        return b==0? a : gcd(b, a%b);
-    }
+//	public int maxPoints(Point[] points) {
+//        if(points == null || points.length == 0) return 0;
+//        
+//        int n = points.length;
+//        if(n == 1 || n == 2) return n;
+//        
+//        int ans = 0;
+//        
+//        for(int i = 0; i < n; i++){
+//            Map<Pair<Integer, Integer>, Integer> map = new HashMap<>();
+//            int samePoint = 1;
+//            int max = 0; // max point in this round
+//            Point p1 = points[i];
+//            for(int j = i + 1; j < n; j++){
+//                Point p2 = points[j];
+//                
+//                // same point 
+//                if(p1.x == p2.x && p1.y == p2.y){
+//                    samePoint++;
+//                }
+//                else{
+//                    Pair key = getSlope(p1, p2);
+//                    if(map.containsKey(key)){
+//                        Integer v = map.get(key);
+//                        v++;
+//                        map.put(key, v);
+//                    }
+//                    else{
+//                        map.put(key, 1);
+//                    }
+//                    max = Math.max(max, map.get(key));
+//                } 
+//                
+//            }
+//            ans = Math.max(ans, max + samePoint);
+//        }
+//        return ans;     
+//    }
+//    
+//    Pair<Integer, Integer> getSlope(Point p1, Point p2){
+//        int dx = p2.x - p1.x;
+//        int dy = p2.y - p1.y;
+//        
+//        // horizantal line ---
+//        if(dy == 0){
+//            return new Pair(p1.y, 0);
+//        }
+//        // vertical line |
+//        if(dx == 0){
+//            return new Pair(0, p1.x);
+//        }
+//        
+//        int gcd = gcd(dx, dy);
+//        return new Pair(dx/gcd, dy/gcd);
+//    }
+//    
+//    int gcd(int a, int b){
+//        return b==0? a : gcd(b, a%b);
+//    }
 	 
 }
