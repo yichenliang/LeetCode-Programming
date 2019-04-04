ec7a43436acd597c687e52d523e138d3a214ac44
yichenliang <yichenliang@ufl.edu>
2019-04-04 18:30:23 -0400

    added on 2019/4/4

---
+++ src/test/java/yichenliang/leetcode/withoutTopic/CourseScheduleTest.java
@@ -0,0 +1,28 @@
+package yichenliang.leetcode.withoutTopic;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CourseScheduleTest {
+	private  CourseSchedule solution;
+
+    @BeforeEach
+    void beforeEach() {
+        solution = new CourseSchedule();
+    }
+    
+    @Test
+    void test1() {
+    	int numCourses = 2;
+    	int[][] prerequisites = {{0,1},{1,0}};
+        Assertions.assertFalse(solution.canFinish(numCourses, prerequisites));
+    }
+    
+    @Test
+    void test2() {
+    	int numCourses = 3;
+    	int[][] prerequisites = {{1,0},{1,2},{0,1}};
+        Assertions.assertFalse(solution.canFinish(numCourses, prerequisites));
+    }
+}
