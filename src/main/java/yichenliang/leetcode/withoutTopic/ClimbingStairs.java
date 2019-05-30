e375e8a21863e2e9289a49618ce5b3d5142dcc59
yichenliang <yichenliang@ufl.edu>
2019-05-29 22:31:53 -0400

    added on 2019/5/29

---
+++ src/main/java/yichenliang/leetcode/withoutTopic/ClimbingStairs.java
@@ -0,0 +1,20 @@
+package yichenliang.leetcode.withoutTopic;
+
+public class ClimbingStairs {
+	
+	public int climbStairs(int n) {
+        
+        if(n == 1){
+            return 1;
+        }
+        
+        int[] dp = new int[n + 1];
+        dp[n - 1] = 1;
+        dp[n - 2] = 2;
+        for(int i = n - 3; i >= 0; i--){
+            dp[i] = dp[i + 1] + dp[i + 2];
+        }
+        return dp[0];
+    }
+
+}
