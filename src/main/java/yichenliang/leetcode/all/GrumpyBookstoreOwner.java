a3948f879a274d7280f56f851875474a880a0b60
yichenliang <yichenliang@ufl.edu>
2019-08-02 12:56:21 +0800

    added on 2019/8/2

---
+++ src/main/java/yichenliang/leetcode/all/GrumpyBookstoreOwner.java
@@ -0,0 +1,54 @@
+package yichenliang.leetcode.all;
+
+/**
+ *  1052. Grumpy Bookstore Owner
+ *  
+ *  Sliding Window
+ *  
+ *  time complexity: O(n)
+ *  space complexity: O(1)
+ *  
+ *  2019/7/13
+ */
+
+public class GrumpyBookstoreOwner {
+
+	// naive solution
+	public int maxSatisfied(int[] customers, int[] grumpy, int X) {
+        int base = 0;
+        int len = customers.length;
+        for(int i = 0; i < len; i++){
+            if(grumpy[i] == 0) base += customers[i];
+        }
+        int res = Integer.MIN_VALUE;
+        for(int i = 0; i <= len - X; i++){
+            int extra = 0;
+            for(int j = i; j < i + X; j++){
+                if(grumpy[j] == 1) extra += customers[j];
+            }
+            res = Math.max(res, base + extra);
+        }
+        return res;
+    }
+	
+	// sliding window
+	public int maxSatisfied1(int[] customers, int[] grumpy, int X) {
+        int base = 0;
+        int len = customers.length;
+        for(int i = 0; i < len; i++){
+            if(grumpy[i] == 0) base += customers[i];
+        }
+        int res = Integer.MIN_VALUE;
+        int extra = 0;
+        for(int i = 0; i < X; i++){
+            if(grumpy[i] == 1) extra += customers[i];
+        }
+        res = Math.max(res, base + extra);
+        for(int i = 1; i <= len - X; i++){
+            if(grumpy[i - 1] == 1) extra -= customers[i - 1];
+            if(grumpy[i + X - 1] == 1) extra += customers[i + X - 1];
+            res = Math.max(res, base + extra);
+        }
+        return res;
+    }
+}
---
+++ src/main/java/yichenliang/leetcode/binarySearch/SquaresOfASortedArray.java
@@ -0,0 +1,60 @@
+package yichenliang.leetcode.binarySearch;
+
+/**
+ * 977. Squares of a Sorted Array
+ * 
+ * Binary Search
+ * 
+ * time complexity: O(n + logn)
+ * space complexity: O(n)
+ * 
+ * 2019/8/2
+ */
+
+public class SquaresOfASortedArray {
+	
+	public int[] sortedSquares(int[] A) {
+        int len = A.length;
+        int[] ans = new int[len];
+        
+        int a = 0, b = len - 1;
+        while(a < b && A[a] < 0){
+            int mid = a + (b - a) / 2;
+            if(A[mid] >= 0){
+                b = mid;
+            }
+            else{
+                a = mid + 1;
+            }
+        }
+        
+        if(a == 0){
+            for(int i = 0; i < len; i++){
+                ans[i] = A[i] * A[i];
+            }
+        }
+        else{
+            b = a - 1;
+            for(int i = 0; i < len; i++){
+                if(b < 0){
+                    ans[i] = A[a] * A[a];
+                    a++;
+                }
+                else if(a >= len){
+                    ans[i] = A[b] * A[b];
+                    b--;
+                }
+                else if(A[b] * A[b] < A[a] * A[a]){
+                    ans[i] = A[b] * A[b];
+                    b--;
+                }
+                else{
+                    ans[i] = A[a] * A[a];
+                    a++;
+                }
+            }
+        }
+        return ans;
+    }
+
+}
