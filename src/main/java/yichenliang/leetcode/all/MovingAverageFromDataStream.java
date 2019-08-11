a87419e17fd75d80d8bef34992ddff81a506416a
yichenliang <yichenliang@ufl.edu>
2019-08-11 11:31:17 +0800

    added on 2019/8/11

---
+++ src/main/java/yichenliang/leetcode/all/MovingAverageFromDataStream.java
@@ -0,0 +1,43 @@
+package yichenliang.leetcode.all;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ *  346. Moving Average from Data Stream
+ *  
+ *  design + queue
+ *  
+ *  time complexity: O(n)
+ *  space complexity: O(size)
+ *  
+ *  2019/8/11
+ * 
+ */
+
+public class MovingAverageFromDataStream {
+	
+	class MovingAverage {
+
+	    Queue<Integer> q;
+	    int size;
+	    int sum;
+	    /** Initialize your data structure here. */
+	    public MovingAverage(int size) {
+	        q = new LinkedList<>();
+	        this.size = size;
+	        sum = 0;
+	    }
+	    
+	    public double next(int val) {
+	        if(q.size() == size){
+	            int former = q.poll();
+	            sum = sum - former;
+	        }
+	        q.offer(val);
+	        sum = sum + val;
+	        double d = (double) q.size();
+	        return sum / d;
+	    }
+	}
+}
